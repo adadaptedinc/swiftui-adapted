2e5235dfe27073202417f7255590cbb30b601333
Brett Clifton <bclifton@adadapted.com>
2024-01-12 15:13:27 -0500

    init commit

---
+++ SwiftUIAdapted/SwiftUIAdapted/SwiftUIAdaptedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdaptedApp.swift
+//  SwiftUIAdapted
+//
+//  Created by Brett Clifton on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdaptedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
